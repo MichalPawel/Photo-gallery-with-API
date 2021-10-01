320c03ad8590ea13d6ea36b169877dc85144db95
Pawel Michalski <pawelmm96@gmail.com>
2021-10-01 23:08:31 +0200

    add class Doggo

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,37 @@
 import '../scss/index.scss';
 
 console.log('Boilerplate is working!');
+
+class Doggo {
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api'
+        this.imgElement = document.querySelector('.featured-dog img')
+        this.init()
+    }
+    listAllBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(response => response.json())
+            .then(data => data.message)
+            .catch(err => console.log(err))
+    }
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`)
+            .then(response => response.json())
+            .then(data => data.message)
+    }
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(res => res.json())
+            .then(data => data.message)
+    }
+    init() {
+        this.getRandomImage()
+            .then(img => this.imgElement.setAttribute('src', img))
+        this.listAllBreeds()
+            .then(list => console.log(list))
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => new Doggo())
+
+
